68bf1452629336834b6002874ee26fa5d8519c36
Soulmate <857153966@qq.com>
2019-03-14 14:03:09 +0800

    增加注释

--- blog-admin-web/src/api/api.js
+++ blog-admin-web/src/api/api.js
@@ -10,7 +10,7 @@ axios.defaults.timeout = 5000;
 //请求头
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 //接口地址
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://localhost:8080';//如果在index中配置了地址，这写请求重复的地方，或者不写
 //静态资源
 Vue.prototype.$static = '';
 
