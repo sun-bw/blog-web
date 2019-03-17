03909fe75db6d7fcbcba49eb786dc1c25ca42c9c
Soulmate <857153966@qq.com>
2019-03-17 12:21:27 +0800

    修改路由，添加ant-design组件

---
+++ blog-admin-web/src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>121312</div>
+</template>
+<script>
+  export default {
+    name:'home',
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+<style>
+
+</style>
--- blog-admin-web/src/router/index.js
+++ blog-admin-web/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
 
+const home = (resolve) => {
+  import('@/components/home').then(module => {
+    resolve(module)
+  })
+};
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
     }
   ]
 })
