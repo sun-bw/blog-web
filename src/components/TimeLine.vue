7e7a1d79fb80bb1d2c324f7d8d29d29c485cf2ed
sunbowen <857153966@qq.com>
2020-02-18 17:32:59 +0800

    时间轴

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,12 +1,19 @@
 <template>
-  <div style="padding: 0 20%;margin-top: 100px">
-    <el-timeline>
-      <el-timeline-item placement="top" v-for="item in data" :key="item.id" :timestamp="item.timelineTime">
-        <el-card>
-          <p>{{item.timelineContent}}</p>
-        </el-card>
-      </el-timeline-item>
-    </el-timeline>
+  <div class="timeline">
+    <ul>
+      <li v-for="(item,index) in data" :key="index" class="timeline-li">
+        <div class="tl-vertical-line"></div>
+        <div class="tl-circle"></div>
+        <div v-if="index % 2 == 0">
+          <div class="tl-content time dd">{{item.timelineTime}}</div>
+          <div class="tl-content timecontent pp" style="top:-20px;">{{item.timelineContent}}</div>
+        </div>
+        <div v-if="index % 2 != 0">
+          <div class="tl-content time pp">{{item.timelineTime}}</div>
+          <div class="tl-content timecontent dd rightContent" style="top:-20px">{{item.timelineContent}}</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -38,5 +45,55 @@ import axios from '@/api/api'
   }
 </script>
 <style scoped>
-
+.timeline{
+  padding: 0 20%;
+  margin-top: 100px
+}
+.timeline-li{
+  position:relative;
+  height:90px;
+  padding-bottom:20px;
+  list-style:none;
+  left:50%
+}
+.tl-vertical-line{
+  border-left:2px solid #23b7e5;
+  position:absolute;
+  height:80px;
+  left:1%;
+  margin-top:20px;
+}
+.tl-circle{
+  border-radius:50%;
+  background-color: #23b7e5;
+  height:12px;
+  width:12px
+}
+.tl-content{
+  height:50px;
+  line-height:50px;
+  position: absolute;
+  font-size:14px;
+}
+.timecontent{
+  background:#23b7e5;
+  min-width:500px;
+  padding: 0 15px;
+  color: #ffffff;
+}
+.time{
+  top:-20px;
+  width:100px;
+  text-align:center;
+}
+.pp{
+  margin-left:20px;
+}
+.dd{
+  right:102%;
+}
+.rightContent{
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
