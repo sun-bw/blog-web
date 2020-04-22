bb9762bac362292254ce081ceb08a7354c3b745a
sunbowen <857153966@qq.com>
2020-04-22 11:06:56 +0800

    talkanymore

--- src/components/Talkanymore.vue
+++ src/components/Talkanymore.vue
@@ -1,22 +1,46 @@
 <template>
     <div class="talkAnymore">
         <Infoleft></Infoleft>
-        <div class="talk-content"></div>
+        <div class="talk-content">
+            <div v-for="(item,index) of data" :key="index" style="border-bottom:1px solid #98a6ad;width:100%">
+                <div class="content">{{item.sayContent}}</div>
+                <img src="../../static/img/2.jpg" style="width:100%">
+                <div style="display:flex">
+                    <div style="flex-grow: 1;">{{item.createTime}}</div>
+                    <div style="flex-grow: 1;">
+                        <img src="../../static/img/like.png">
+                    </div>
+                    <div style="flex-grow: 1;">
+                        <i class="el-icon-chat-dot-square"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 import Infoleft from './Infoleft'
+import axios from '../api/api.js'
 export default {
     components:{
-        Infoleft
+        Infoleft 
     },
     data(){
         return{
-
+            data:[],
         }
     },
+    mounted(){
+        this.findTalk();
+    },
     methods:{
-
+        // 查询
+        findTalk(){
+            axios.post("/say/findSayContent","").then(res => {
+                console.log(res)
+                this.data = res.data
+            })
+        }
     }
 }
 </script>
@@ -33,5 +57,14 @@ export default {
     width: 75%;
     background: #fff;
     border-radius: 20px;
+    padding: 20px 20px;
+}
+.content{
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    font-size: 11px;
+    color: #999999;
+    line-height: 25px;
+    letter-spacing: 1px;
+    text-align: right;
 }
 </style>
