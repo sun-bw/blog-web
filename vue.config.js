aba5739f1dedfef6176f3195f448835b3ff06771
sun <857153966@qq.com>
2020-08-01 13:19:58 +0800

    build：修改打包路径

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
 //添加别名
 const resolve = (dir) => path.join(__dirname, dir);
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/static' : '/',  // 公共路径
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',  // 公共路径
     indexPath: 'index.html' , // 相对于打包路径index.html的路径
     outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
     assetsDir: 'static', // 相对于outputDir的静态资源(js、css、img、fonts)目录
